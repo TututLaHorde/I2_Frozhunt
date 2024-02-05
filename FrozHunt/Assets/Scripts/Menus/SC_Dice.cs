7c8f387f1f8d7abebd852809178940bc97758a9e
wighthood <zefires@gmail.com>
2024-02-05 10:49:27 +0100

    added comment

--- FrozHunt/Assets/Scripts/Menus/SC_Dice.cs
+++ FrozHunt/Assets/Scripts/Menus/SC_Dice.cs
@@ -15,6 +15,7 @@ public class SC_Dice : MonoBehaviour
     
     private void Start()
     {
+        //get value of Scriptable Object
         m_dice = m_soDice.m_dice;
         m_maxTime = m_soDice.m_maxTime;
     }
@@ -24,7 +25,9 @@ public class SC_Dice : MonoBehaviour
         result = Random.Range(1, m_dice.Length+1);
         if (TryGetComponent(out SpriteRenderer sp))
         {
+            //get SpriteRenderer
             m_sprites = sp;
+            //check that list is not empty
             Assert.IsNotNull(m_dice, "error : empty list");
             StartCoroutine(Dice(result));
         }
@@ -39,6 +42,7 @@ public class SC_Dice : MonoBehaviour
         float timeleft = m_maxTime;
         while (timeleft >= 0.0f)
         {
+            //animate dice
             float deltaTime = m_animCurve.Evaluate(1-timeleft / m_maxTime);
             timeleft -= deltaTime;
             m_result = Random.Range(0, m_dice.Length);
@@ -46,6 +50,7 @@ public class SC_Dice : MonoBehaviour
             m_sprites.sprite = m_sprite;
             yield return new WaitForSeconds(deltaTime);
         }
+        //display finalresult
         m_result = result-1;
         m_sprite = m_dice[m_result];
         m_sprites.sprite = m_sprite;
