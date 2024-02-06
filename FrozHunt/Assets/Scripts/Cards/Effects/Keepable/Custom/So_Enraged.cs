f666a0307020b0de4f32f7d2796ac84189ca58e2
Mathieu Laval <m.laval@lahorde.school>
2024-02-06 14:59:16 +0000

    end shiny pebbles

---
+++ FrozHunt/Assets/Scripts/Cards/Effects/Keepable/Custom/So_Enraged.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class So_Enraged : So_Passive
+{
+    public override void UseEffect() { }
+
+    public override void SelectedCard(GameObject owner)
+    {
+        base.SelectedCard(owner);    
+    }
+}
--- FrozHunt/Assets/Scripts/Cards/Effects/Keepable/Custom/So_ShinyPebbles.cs
+++ FrozHunt/Assets/Scripts/Cards/Effects/Keepable/Custom/So_ShinyPebbles.cs
@@ -8,4 +8,20 @@ public class So_ShinyPebbles : So_Keepable
     public override void UseEffect()
     {
     }
+
+    public override void SelectedCard(GameObject owner)
+    {
+        bool canSet = Sc_BoardManager.Instance.AddEffectCardInHand(this);
+        Sc_PbCard c = owner.GetComponent<Sc_PbCard>();
+
+        if (canSet)
+        {
+            Sc_BoardManager.Instance.InstantiateBonusCard(this);
+            Sc_BoardManager.Instance.RemoveAllPrefabCardWithDiscardIndex(c.m_indexPosition);
+        }
+        else
+        {
+            Sc_BoardManager.Instance.RemoveAllPrefabCardWithout(c.m_indexPosition);
+        }
+    }
 }
