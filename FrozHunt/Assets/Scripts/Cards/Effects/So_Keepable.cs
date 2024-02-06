1a815ad67cafc6d29638e358a32c608ffa37a25b
Mathieu Laval <m.laval@lahorde.school>
2024-02-06 13:45:40 +0000

    player Hand finish

--- FrozHunt/Assets/Scripts/Cards/Effects/So_Keepable.cs
+++ FrozHunt/Assets/Scripts/Cards/Effects/So_Keepable.cs
@@ -7,6 +7,22 @@ public class So_Keepable : So_Effect
 {
     public override void SelectedCard(GameObject owner)
     {
+        bool canSet = Sc_BoardManager.Instance.AddEffectCardInHand(this);
+        Sc_PbCard c = owner.GetComponent<Sc_PbCard>();
 
+        if (canSet)
+        {
+            Sc_BoardManager.Instance.InstantiateBonusCard(this);
+            Sc_BoardManager.Instance.RemoveAllPrefabCardWithDiscardIndex(c.m_indexPosition);
+        }
+        else
+        {
+            Sc_BoardManager.Instance.RemoveAllPrefabCard();
+        }
+    }
+
+    public override void UseEffect()
+    {
+        Debug.Log("Use Effect");
     }
 }
--- FrozHunt/Assets/Scripts/Cards/Sc_BoardManager.cs
+++ FrozHunt/Assets/Scripts/Cards/Sc_BoardManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sc_BoardManager : MonoBehaviour
 {
     public List<So_Card> m_deck;
-    public List<So_Card> m_cards;
+    public List<So_Card> m_boardCards;
+    public List<So_Card> m_discardDeck;
     public List<So_Effect> m_effectCard;
 
     public List<GameObject> m_cardPrefabEmplacements;
@@ -15,8 +19,7 @@ public class Sc_BoardManager : MonoBehaviour
     public bool m_blendInStart = false;
     public SC_Dice m_dice;
 
-    private List<So_Card> m_discardDeck = new List<So_Card>();
-    private int m_bonusCardNumber = 0;
+    public int m_bonusCardNumber = 0;
 
     public static Sc_BoardManager Instance;
 
@@ -30,7 +33,6 @@ public class Sc_BoardManager : MonoBehaviour
             BlendDeck();
     }
 
-    [ContextMenu("Blend deck")]
     public void BlendDeck()
     {
         m_deck = m_deck.OrderBy(x => Random.Range(0, m_deck.Count)).ToList();
@@ -38,6 +40,9 @@ public class Sc_BoardManager : MonoBehaviour
 
     public void GetCard()
     {
+        if (HasCardInBoard())
+            return;
+
         int r = 0;
         float t = m_dice.ThrowDice(ref r);
         StartCoroutine(GetCardAfterTimer(t + .5f, r));
@@ -51,18 +56,31 @@ public class Sc_BoardManager : MonoBehaviour
     {
         for (int i = 0; i < n; i++)
         {
-            m_discardDeck.Add(m_deck[0]);
-            m_cards.Add(m_deck[0]);
+            m_boardCards.Add(m_deck[0]);
 
             GameObject e = m_cardPrefabEmplacements[i];
             GameObject inst = Instantiate(m_deck[0].m_prebafInBoard, e.transform);
             Sc_PbCard c = inst.GetComponent<Sc_PbCard>();
             c.InitDisplayCard(m_deck[0]);
+            c.m_indexPosition = i;
 
             m_deck.RemoveAt(0);
             if (m_deck.Count < 1)
                 SwitchDescardToDeck();
         }
+    }
+
+    public bool HasCardInBoard()
+    {
+        foreach (var item in m_cardPrefabEmplacements)
+        {
+            int childCount = item.transform.childCount;
+
+            if (childCount > 0)
+                return true;
+        }
+
+        return false;
     }
 
     public void SwitchDescardToDeck()
@@ -75,15 +93,38 @@ public class Sc_BoardManager : MonoBehaviour
 
     public void RemoveAllPrefabCard()
     {
-        foreach (var item in m_cardPrefabEmplacements)
+        foreach (var item in m_cardPrefabEmplacements.Select((v, i) => new {v, i}))
         {
-            if (item.transform.childCount < 1)
+            if (item.v.transform.childCount < 1)
                 return;
 
-            GameObject g = item.transform.GetChild(0).gameObject;
+            GameObject g = item.v.transform.GetChild(0).gameObject;
             if (g)
             {
                 g.GetComponent<MonoBehaviour>().StopAllCoroutines();
+                
+                m_discardDeck.Add(m_boardCards[0]);
+                m_boardCards.RemoveAt(0);
+                Destroy(g);
+            }
+        }
+    }
+    public void RemoveAllPrefabCardWithDiscardIndex(int index)
+    {
+        foreach (var item in m_cardPrefabEmplacements.Select((v, i) => new { v, i }))
+        {
+            if (item.v.transform.childCount < 1)
+                return;
+
+            GameObject g = item.v.transform.GetChild(0).gameObject;
+            if (g)
+            {
+                g.GetComponent<MonoBehaviour>().StopAllCoroutines();
+
+                if (!item.i.Equals(index))
+                    m_discardDeck.Add(m_boardCards[0]);
+
+                m_boardCards.RemoveAt(0);
                 Destroy(g);
             }
         }
@@ -97,6 +138,46 @@ public class Sc_BoardManager : MonoBehaviour
                 Destroy(e);
         }
     }
+    public void RemoveBonusCard(int i)
+    {
+        int childCount = m_bonusCardPrefabEmplacements[i].transform.childCount;
+
+        if (childCount > 0)
+        {
+            GameObject e = m_bonusCardPrefabEmplacements[i].transform.GetChild(0).gameObject;
+            m_discardDeck.Add(m_effectCard[i]);
+            m_effectCard.RemoveAt(i);
+            m_bonusCardNumber--;
+
+            Destroy(e);
+            Invoke(nameof(ReparentBonusCard), Time.deltaTime);
+        }
+    }
+
+    public void ReparentBonusCard()
+    {
+        foreach (var item in m_bonusCardPrefabEmplacements.Select((v, i) => new { v, i }))
+        {
+            if (item.i + 1 > 2)
+                return;
+
+            int currentChildCount = item.v.transform.childCount;
+
+            if (currentChildCount < 1)
+            {
+                int nextChildCount = m_bonusCardPrefabEmplacements[item.i + 1].transform.childCount;
+
+                if (nextChildCount > 0)
+                {
+                    Transform move = m_bonusCardPrefabEmplacements[item.i + 1].transform.GetChild(0);
+                    RectTransform t = move.GetComponent<RectTransform>();
+
+                    move.SetParent(item.v.transform);
+                    t.localPosition = Vector3.zero;
+                }
+            }
+        }
+    }
 
     public bool AddEffectCardInHand(So_Effect e)
     {
@@ -106,10 +187,20 @@ public class Sc_BoardManager : MonoBehaviour
         m_effectCard.Add(e);
         return true;
     }
-
     public void InstantiateBonusCard(So_Effect e)
     {
+        GameObject g = m_bonusCardPrefabEmplacements[m_bonusCardNumber];
+        GameObject inst = Instantiate(e.m_prebafInBoard, g.transform);
 
+        Sc_PbCard c = inst.GetComponent<Sc_PbCard>();
+        Sc_HandCard h = g.GetComponent<Sc_HandCard>();
+        Button b = inst.GetComponent<Button>();
+
+        b.enabled = false;
+        h.m_effectCard = e;
+        c.InitDisplayCard(e);
+
+        m_bonusCardNumber++;
     }
 
     public void SetEnableCard(bool enable)
