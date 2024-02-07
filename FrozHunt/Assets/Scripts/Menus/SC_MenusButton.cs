22bf6e36fb2511f3cedfa4d0ccd9048067799d12
wighthood <zefires@gmail.com>
2024-02-07 12:19:41 +0100

    expanded char select to 4
    can now select up to 4 character with 6 different classes without duplicate and properly saving player char choice

--- FrozHunt/Assets/Scripts/Menus/SC_MenusButton.cs
+++ FrozHunt/Assets/Scripts/Menus/SC_MenusButton.cs
@@ -7,6 +7,7 @@ public class SC_MenusButton : MonoBehaviour
     [SerializeField] private GameObject m_pauseMenu;
     [SerializeField] private List<So_CardPlayer> m_players = new();
     public List<Sc_PlayerCardControler> m_playersControler = new();
+
     public void LoadMainMenu()
     {
         SceneManager.LoadScene("MainMenuScene"); 
@@ -18,7 +19,7 @@ public class SC_MenusButton : MonoBehaviour
     }
     public void LoadLore()
     {
-        for(int i = 0; i < m_playersControler.Count; i++) 
+        for(int i = 0; i < SC_PlayerCount.instance.m_count ; i++) 
         {
             m_players.Add(m_playersControler[i].m_CardInfo);
         }
