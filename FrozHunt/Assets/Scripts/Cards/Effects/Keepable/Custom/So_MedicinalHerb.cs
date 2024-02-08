ec890addcfc36f31000ec4e8bb99dac7d0e11f78
shenanigan11 <enzo2dessaint@gmail.com>
2024-02-08 12:20:16 +0100

    4player

--- FrozHunt/Assets/Scripts/Cards/Effects/Keepable/Custom/So_MedicinalHerb.cs
+++ FrozHunt/Assets/Scripts/Cards/Effects/Keepable/Custom/So_MedicinalHerb.cs
@@ -10,9 +10,6 @@ public class So_MedicinalHerb : So_Consumable
         Sc_PopUpManager.Instance.HealPopUp.SetActive(true);
         Sc_PopUpManager.Instance.SetHealthValue(m_healValue);
 
-        Sc_PopUpManager.Instance.SetHealPopUp(
-            Sc_GameManager.Instance.playerList[0],    
-            Sc_GameManager.Instance.playerList[1], true    
-        );
+        Sc_PopUpManager.Instance.SetHealPopUp(true);
     }
 }
