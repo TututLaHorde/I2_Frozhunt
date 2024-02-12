a926bf82cd899f8399ec7baf926f4086512923f0
wighthood <zefires@gmail.com>
2024-02-12 14:25:36 +0100

    remade player card

--- FrozHunt/Assets/Scripts/Players/So_CardPlayer.cs
+++ FrozHunt/Assets/Scripts/Players/So_CardPlayer.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
 using UnityEngine;
 
 public enum PlayerCard
 {
-    Massue, Druide, Lance, test1, test2, test3
+    Gada, Muni, Sula, test1, test2, test3
 }
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/PlayerCardInfo", order = 1)]
@@ -17,6 +14,5 @@ public class So_CardPlayer : ScriptableObject
     public int damage;
     public Sprite cardArt;
     public string description;
-    public string attackName;
-    
+    public string attackName; 
 }
