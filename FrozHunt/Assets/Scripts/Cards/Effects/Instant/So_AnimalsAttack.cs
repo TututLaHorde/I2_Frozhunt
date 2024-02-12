d4c2c1673a6d589577b4bba2758586c9a4288786
Mathieu Laval <m.laval@lahorde.school>
2024-02-12 17:06:12 +0100

    refont animalAttack card

--- FrozHunt/Assets/Scripts/Cards/Effects/Instant/So_AnimalsAttack.cs
+++ FrozHunt/Assets/Scripts/Cards/Effects/Instant/So_AnimalsAttack.cs
@@ -4,27 +4,22 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AnimalATK", menuName = "Card/Instant/AnimalATK")]
 public class So_AnimalsAttack : So_Instant
 {
-    public int m_numberOfDamage = 2; // for each food you don't have
-    public int m_numberOfLostFood = 2;
+    public int m_numberOfDamage = 4;
     public override void SelectedCard(GameObject owner)
     {
-        for (int i = 0; i < m_numberOfLostFood; i++) 
+        if (Sc_GameManager.Instance.GetFood() > 1)
         {
-            if(Sc_GameManager.Instance.GetFood() > 0 )
+            Sc_GameManager.Instance.AddFood(-2);
+        }
+        else
+        {
+            for (int j = 0; j < Sc_GameManager.Instance.playerList.Count; j++)
             {
-                Sc_GameManager.Instance.AddFood(-1);
-            }
-            else 
-            {
-                for(int j = 0;  j < Sc_GameManager.Instance.playerList.Count; j++) 
-                {
-                    Debug.Log("Give 2 Damage to player index  " + j);
-                    Sc_GameManager.Instance.playerList[j].TakeDamage(m_numberOfDamage);
-                }
+                Debug.Log("Give 2 Damage to player index  " + j);
+                Sc_GameManager.Instance.playerList[j].TakeDamage(m_numberOfDamage);
             }
         }
         
-
         base.SelectedCard(owner);
     }
 }
