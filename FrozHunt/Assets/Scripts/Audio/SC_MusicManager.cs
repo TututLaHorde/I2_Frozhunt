745835d7dd0975f34066d85872bc78194a3c8446
wighthood <zefires@gmail.com>
2024-02-14 12:28:03 +0100

    added fade in/ fade out
    sound fade in and out before changing
    added ambient sound changing
    modified how the music is changed

--- FrozHunt/Assets/Scripts/Audio/SC_MusicManager.cs
+++ FrozHunt/Assets/Scripts/Audio/SC_MusicManager.cs
@@ -1,24 +1,47 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Audio;
 
+public enum WindEffect
+{
+    lightWind, StrongWind,
+}
+
+public enum MenuMusic
+{
+    menu1,menu2, victoire, gameover
+}
+
 public class SC_MusicManager : MonoBehaviour
 {
-    [SerializeField] private AudioClip[] playlist;
-    [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioMixerGroup m_SFXgroup;
 
+    [Header ("Ambient")]
+    [SerializeField] private AudioSource m_ambientSource;
+    [SerializeField] private AudioClip[] m_ambientClip;
+
+    [Header ("Music")]
+    [SerializeField] private AudioSource m_musicSource;
+    [SerializeField] private AudioClip[] m_menuClip;
+
+    [Header ("AudioFade")]
+    [SerializeField] private float timer;
+
     public static SC_MusicManager Instance;
-    public void ChangeMusic(int arraypos)
+    public void ChangeMusic(AudioClip clip)
     {
-        if(arraypos < playlist.Length)
-        {
-            audioSource.clip = playlist[arraypos];
-            audioSource.Play();
-        }
-        else
-        {
-            print("out of bounds");
-        }
+        StartCoroutine(AudioFade(m_musicSource, clip));
+    }
+
+    public void menuMusic(MenuMusic menumusic)
+    {
+        StartCoroutine(AudioFade(m_musicSource, m_menuClip[menumusic.GetHashCode()]));
+    }
+
+
+    public void ChangeAmbient(WindEffect windEffect)
+    {
+        StartCoroutine(AudioFade(m_ambientSource, m_ambientClip[windEffect.GetHashCode()]));
     }
 
     void Awake()
@@ -30,24 +53,48 @@ public class SC_MusicManager : MonoBehaviour
         }
         Instance = this;
         GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
-
         if (objs.Length > 1)
         {
             Destroy(gameObject);
         }
-
         DontDestroyOnLoad(gameObject);
     }
 
     public AudioSource PlayClipAt(AudioClip clip)
     {
+        //create tempaudio object
         GameObject tempGO = new GameObject("TempAudio");
         tempGO.transform.localPosition = Vector3.zero;
+        //assign to temp the audiosource
         AudioSource SFXsource = tempGO.AddComponent<AudioSource>();
+        //make temp play sound
         SFXsource.clip = clip;
-        SFXsource.outputAudioMixerGroup = m_SFXgroup;
+        SFXsource.outputAudioMixerGroup = m_SFXgroup;     
         SFXsource.Play();
+        //destroy temp once it's done;
         Destroy(tempGO, clip.length);
         return SFXsource;
+    }
+
+    private IEnumerator AudioFade(AudioSource audioSource, AudioClip clip)
+    {
+        float time = timer;
+        while (time >= 0)
+        {
+            //lower volume until it's muted
+            time -= Time.deltaTime;
+            audioSource.volume = time / timer;
+            yield return null;
+        }
+        //change clip
+        audioSource.clip = clip;
+        audioSource.Play();
+        while (time <= timer)
+        {
+            //increase volume until it's max volume
+            time += Time.deltaTime;
+            audioSource.volume = time / timer;
+            yield return null;
+        }
     }
 }
