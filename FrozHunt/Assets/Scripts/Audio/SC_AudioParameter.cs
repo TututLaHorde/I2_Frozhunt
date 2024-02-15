b90d285b7e53f441dcc86270e1fba89e42c55a19
wighthood <zefires@gmail.com>
2024-02-15 12:21:12 +0100

    added assets to settings menu
    also made it so the value of the slider carry over

--- FrozHunt/Assets/Scripts/Audio/SC_AudioParameter.cs
+++ FrozHunt/Assets/Scripts/Audio/SC_AudioParameter.cs
@@ -1,13 +1,34 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SC_AudioParameter : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    [SerializeField] private Slider SliderMasters;
+    [SerializeField] private Slider SliderMusic;
+    [SerializeField] private Slider SliderAmbient;
     public void setMastervolume(float volume)
     {
         audioMixer.SetFloat("Master", volume);
     }
+
+    private void OnEnable()
+    {
+        if (audioMixer.GetFloat("Master", out float Mastervalue))
+        {
+            SliderMasters.SetValueWithoutNotify(Mastervalue);
+        }
+        if (audioMixer.GetFloat("Music", out float Musicvalue))
+        {
+            SliderMusic.SetValueWithoutNotify(Musicvalue);
+        }
+        if (audioMixer.GetFloat("Ambient", out float value))
+        {
+            SliderAmbient.SetValueWithoutNotify(value);
+        }
+    }
+
 
     public void setMusicvolume(float volume)
     {
